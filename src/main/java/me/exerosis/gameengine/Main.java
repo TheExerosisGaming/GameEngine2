b14dab64baf3c45b88f4f48188b458ef352f8a26
Bench <BinaryBench@gmail.com>
2016-03-17 02:42:36 -0400

    FIRST!

---
+++ src/main/java/me/exerosis/gameengine/Main.java
@@ -0,0 +1,9 @@
+package me.exerosis.gameengine;
+
+/**
+ * Created by Bench on 3/17/2016.
+ */
+public class Main {
+
+
+}
---
+++ src/main/java/me/exerosis/gameengine/arena/Arena.java
@@ -0,0 +1,9 @@
+package me.exerosis.gameengine.arena;
+
+/**
+ * Created by Bench on 3/17/2016.
+ */
+public class Arena {
+
+
+}
